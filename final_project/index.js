5fd8bb2b52cb08d5e0aec126099a3105f4aa3b23
paveltrninkov <pavel.trninkov@hotmail.com>
2024-11-13 20:08:23 -0500

    did a bit of stuff

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,26 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', {expiresIn: 60*60})
+
+        req.session.authorization = {
+            accessToken, username
+        }
+
+        return res.status(200).send("User successfully authenticated");
+    } else {
+        return res.status(208).json({message: "Invalid authentication"})
+    }
 });
  
 const PORT =5000;
